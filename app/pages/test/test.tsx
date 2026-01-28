55a3f6c65ef049c0a4dab9787e24592d9bf1b4b0
iTzMeRafa <rafael.carneiro@web.de>
2026-01-28 14:49:27 +0100

    feat: appwrite connection

--- app/pages/test/test.tsx
+++ app/pages/test/test.tsx
@@ -1,5 +1,29 @@
-export function Test() {
-  return (
-   <h1>Test page</h1>
-  );
+import { client } from 'app/lib/appwrite';
+
+export async function loader() {
+	return null;
+}
+
+export default function Test() {
+	return (
+		<div>
+			<h1>test page</h1>
+			<button
+				onClick={async () => {
+					const result = await client.ping();
+					const log = {
+						date: new Date(),
+						method: 'GET',
+						path: '/v1/ping',
+						status: 200,
+						response: JSON.stringify(result),
+					};
+					console.log('log');
+					console.log(log);
+				}}
+			>
+				ping appwrite
+			</button>
+		</div>
+	);
 }
